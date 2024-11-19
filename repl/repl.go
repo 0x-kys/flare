c2c7df3a11b3ea11be7e57df489445aafff34178
for { kys() } <yyeet62425@gmail.com>
2024-11-19 13:53:36 +0530

    evaluation - mid

--- repl/repl.go
+++ repl/repl.go
@@ -2,6 +2,7 @@ package repl
 
 import (
 	"bufio"
+	"flare/evaluator"
 	"flare/lexer"
 	"flare/parser"
 	"fmt"
@@ -36,8 +37,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
